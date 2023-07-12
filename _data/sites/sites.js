c402924a0f6ebd9ad0e6856f7ec15447e5df623e
Dom Wakeling <domwakeling@gmail.com>
2023-07-12 11:53:25 +0000

    Add actual sites, remove welcoming guide

---
+++ _data/sites/sites.js
@@ -0,0 +1,20 @@
+module.exports = {
+	description: "Dom's websites",
+	skip: false,
+	options: {
+		frequency: 60 * 23, // 23 hours
+		// Use "run" if the sites don’t share assets on the same origin
+		//           and we can reset chrome with each run instead of
+		//           each site in every run (it’s faster)
+		// Use "site" if sites are all on the same origin and share assets.
+		freshChrome: "run"
+	},
+	urls: [
+		"https://www.dolwensolutions.com",
+		"https://www.bowlesskiracingclub.org.uk/",
+		"https://lsersa.org/",
+		"https://distracted-elion-60a106.netlify.app/",
+		"https://delegate-site.netlify.app/",
+		"https://stunning-torte-f00404.netlify.app/"
+	]
+};
